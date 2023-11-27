ea71b79cf40bd40e3efabcdf2622095881c9c77c
Edson Júnior <edsonjunior.1219@gmail.com>
2023-11-27 15:48:54 -0300

    refac(src/index.ts): get port from env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // src/index.ts
 import app from './app';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
